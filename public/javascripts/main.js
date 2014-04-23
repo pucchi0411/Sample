9880e4a0a00385c531618066c89649a4a4600696
pucchi0411 <pucchi0411@gmail.com>
2014-04-24 01:53:10 +0900

    自動リロード導入

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,26 @@
-$(function(){
+$(function () {
 
-    $("body").keydown(function(event){
-        if( event.ctrlKey === true && event.which === 13 ){
+    $("body").keydown(function (event) {
+        if (event.ctrlKey === true && event.which === 13) {
             $("form").submit();
         }
     });
 
+    setTimeout(refresh(), 1000);
 });
+
+function refresh() {
+    var time = new Date().getTime();
+
+    var reloader =  (function() {
+        if (new Date().getTime() - time >= 5000) {
+            console.log("reloaded");
+            window.location.reload(true);
+        } else {
+            console.log("interval time not yet.");
+            setTimeout(reloader, 1000);
+        }
+    });
+
+    return reloader;
+}
