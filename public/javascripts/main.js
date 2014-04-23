55e37983b42c754254af4b0bb91eb5771303c1db
pucchi0411 <pucchi0411@gmail.com>
2014-04-24 00:11:45 +0900

    Ctrl+Enterによる投稿

---
+++ public/javascripts/main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+    $("body").keydown(function(event){
+        if( event.ctrlKey === true && event.which === 13 ){
+            console.log("press control + enter");
+            $("form").submit();
+        }
+    });
+
+    console.log("hello");
+});
